e012cb377573c356eb0322675c3d3b76bef56622
Vinay <vnchoudhary357@gmail.com>
2020-05-28 22:23:15 +0530

    added ex_hidden.py

---
+++ example_hidden.py
@@ -0,0 +1,8 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+key = (	"wc0xelBgDySclrW64xxxxxxxxxxxxx",
+		"9blIEPIF8PgaMqxrbVfEzJFHrhwftLp5Dxxxxxxxxxxxx",
+		"2886543703-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		"qEll8EXk08IhQFxxxxxxxxxxxxxxxxxxxxxx")
